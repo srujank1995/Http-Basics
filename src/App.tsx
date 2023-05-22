cea5cc1caa5df6a47cfd18fd3b0e8ad480769299
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-05-22 21:19:34 +0530

    Axios API Used

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-const  App = () => {
+const App = () => {
+  const [users, setUsers] = useState([]);
 
-  useEffect(()=>{
-    axios.get("http://localhost:3001/users").then(
-      (res) =>{console.log("res", res)}
-
-    ).catch((err) =>{console.log("err", err)} )      
-  },[])
-  return (
-    <div>
-      HTTPs
-    </div>
-  );
-}
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/users")
+      .then((res) => {
+        const { allUsers } = res.data;
+        console.log("res", res.data);
+        setUsers(allUsers);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }, []);
+  const Userlog = users.map((usr: any, index) => {
+    return (
+      <p key={usr.index}>
+        User:{index} == {usr.email}
+      </p>
+    );
+  });
+  return <div><h2>IDs List</h2> {Userlog}</div>;
+};
 
 export default App;
